14fbaa39521edf330f35ef8462d2873a2a7274d4
bekolozi <bekachkhirodze1@gmail.com>
2025-03-06 18:49:47 +0400

    Update CORS configuration to allow x-csrf-token header

--- src/index.js
+++ src/index.js
@@ -55,7 +55,12 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: [
+    'Content-Type', 
+    'Authorization', 
+    'x-csrf-token',
+    'csrf-token'
+  ],
   exposedHeaders: ['Content-Range', 'X-Total-Count'],
   maxAge: 86400
 };
